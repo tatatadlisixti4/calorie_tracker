5cc819a8b2827c734f7a8a54fae608778abbda59
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-03 21:50:21 -0300

    Añadiendo más información al listado de actividades

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,4 +1,7 @@
+import {useMemo} from "react";
 import {Activity} from "../types";
+import {categories} from "../data/categories.ts";
+
 
 type ActivityListProps = {
     activities: Activity[]
@@ -6,19 +9,29 @@ type ActivityListProps = {
 
 
 export default function ActivityList({activities}: ActivityListProps) {
-    console.log(activities)
+    const categoryName = useMemo(() =>
+            (category: Activity['category']) => categories[category-1].name
+
+        // categories[+activity.category -1][1]
+    , [activities])
+
     return(
         <>
             <h2 className="text-4xl font-bold text-slate-600 text-center">Comida y Actividades</h2>
             {activities.map( activity => (
                 <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                     <div className="space-y-2 relative">
-                        <p> {activity.category} </p>
+                        <p
+                            className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}
+                        >
+                            {categoryName(+activity.category)}
+                        </p>
                         <p className="text-2xl font-bold pt-5">{activity.name}</p>
                         <p className="font-black text-4xl text-lime-500">
                             {activity.calories} {''}
                             <span>Calorias</span>
                         </p>
+
                     </div>
 
                     <div>
