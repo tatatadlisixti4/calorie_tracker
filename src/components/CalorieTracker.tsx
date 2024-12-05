0d4f7be7888a6c117a3fcc4efa561f134f3ecef5
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-05 18:28:05 -0300

    Mostrando las calorías consumidas

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,10 +1,26 @@
+import { useMemo } from "react"
 import type{ Activity } from "../types"
 
 type CalorieTrackerProps = {
     activities: Activity[]
 }
 export default function CalorieTracker({activities} : CalorieTrackerProps) {
+
+    const caloriesConsumed = useMemo(() => activities.reduce(
+        (total, activity) => activity.category===1? total + activity.calories : total, 0) 
+    , [activities])
+
     return (
-        <div>CalorieTracker</div>
+        <>
+            <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
+            <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+                <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
+                    <span className="font-black text-6xl text-orange">{caloriesConsumed}</span>
+                    Consumidas
+                </p>
+            </div>
+
+            
+        </>
     )
 }
