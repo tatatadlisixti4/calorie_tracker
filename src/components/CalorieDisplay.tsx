26b406e688c2e363167689bcd25281f21b77e099
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-05 18:44:26 -0300

    Build y deployment de la app

--- src/components/CalorieDisplay.tsx
+++ src/components/CalorieDisplay.tsx
@@ -6,7 +6,7 @@ type CalorieDisplayProps = {
 export default function CalorieDisplay({calories, text}: CalorieDisplayProps) {
     return (
         <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-            <span className="font-black text-6xl text-orange">{calories}</span>
+            <span className="font-black text-6xl text-orange-500">{calories}</span>
             {text}
         </p>
     )
