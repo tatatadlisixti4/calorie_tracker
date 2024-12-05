057b616cb28dacd735b6ab501e55d7f39575e876
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-05 18:05:13 -0300

    Creando un componente para mostrar el total de calorias

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import {useReducer, useEffect, useMemo} from "react";
 import {Form} from "./components/Form.tsx";
 import ActivityList from "./components/ActivityList.tsx";
+import CalorieTracker from "./components/CalorieTracker.tsx";
+
 import {activityReducer, initialState} from "./reducers/activity-reducer.ts";
+
 
 function App() {
     // Hooks
@@ -42,6 +45,15 @@ function App() {
                     />
                 </div>
             </section> {/* Fin Componente Form */}
+
+            <section className="bg-gray-800 py-10">
+                <div className="max-w-4xl mx-auto">
+                    <CalorieTracker 
+                        activities={state.activities}
+                    />
+                </div>
+            </section>
+
 
             {/* Componente ActivityList */}
             <section className="p-10 mx-auto max-w-4xl">
